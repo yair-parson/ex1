7b3e653d0f7c4a435c51000e11ed3b6dfc08c44f
yair <yairparson@gmail.com>
2023-11-11 17:40:44 +0200

    adding the declarations to the hara

--- RLEList.c
+++ RLEList.c
@@ -2,6 +2,18 @@
 
 struct RLEList_t{
     //TODO: implement
+    // methods
+    RLEList RLEListCreate();
+    void RLEListDestroy(RLEList list);
+    RLEListResult RLEListAppend(RLEList list, char value);
+    int RLEListSize(RLEList list);
+    RLEListResult RLEListRemove(RLEList list, int index);
+    char RLEListGet(RLEList list, int index, RLEListResult *result)
+    char* RLEListExportToString(RLEList list, RLEListResult* result);
+    RLEListResult RLEListMap(RLEList list, MapFunction map_function);
+    //attribuites
+    
+    
 };
 
 //implement the functions here
