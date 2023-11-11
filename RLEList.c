a2ac0099de85f31aa6e1a24c46cc0094e58c57b6
yair <yairparson@gmail.com>
2023-11-11 17:46:48 +0200

    addedd struct fields, and arranged the file to start implementing the functions

--- RLEList.c
+++ RLEList.c
@@ -2,18 +2,28 @@
 
 struct RLEList_t{
     //TODO: implement
-    // methods
-    RLEList RLEListCreate();
-    void RLEListDestroy(RLEList list);
-    RLEListResult RLEListAppend(RLEList list, char value);
-    int RLEListSize(RLEList list);
-    RLEListResult RLEListRemove(RLEList list, int index);
-    char RLEListGet(RLEList list, int index, RLEListResult *result)
-    char* RLEListExportToString(RLEList list, RLEListResult* result);
-    RLEListResult RLEListMap(RLEList list, MapFunction map_function);
-    //attribuites
-    
-    
+    char c;
+    int appear;
+    RLEList* next;
 };
 
 //implement the functions here
+
+RLEList RLEListCreate();
+
+void RLEListDestroy(RLEList list);
+
+RLEListResult RLEListAppend(RLEList list, char value);
+
+int RLEListSize(RLEList list);
+
+RLEListResult RLEListRemove(RLEList list, int index);
+
+char RLEListGet(RLEList list, int index, RLEListResult *result)
+
+char* RLEListExportToString(RLEList list, RLEListResult* result);
+
+RLEListResult RLEListMap(RLEList list, MapFunction map_function);
+
+
+
